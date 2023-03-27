f9b30a8224268667b598e737b5dd3d3c7635cf16
k.roman <romaknyazev55@yandex.ru>
2023-03-27 22:14:34 +0300

    1

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-import dataclasses
-from pathlib import Path
-
-
-def scanner(root: str, depth: int):
-    level_patterns = [f'.{"/*" * i}' for i in range(1, depth + 1)]
-
-    def _scan():
-        result = []
-        for level_index, level_pattern in enumerate(level_patterns):
-            level = set()
-            for path in Path(root).glob(level_pattern):
-                (level_index == 0 or (path.parent in result[level_index - 1])) and level.add(path)
-            result.append(level)
-        return result
-
-    return _scan
-
-
-@dataclasses.dataclass
-class ScanDiff:
-    created: set[Path]
-    deleted: set[Path]
-
-
-def diff(prev_scan: list[set[Path]] | None, current_scan: list[set[Path]]):
-    prev_scan = prev_scan or [set()] * len(current_scan)
-    return [ScanDiff(created=curr - prev,
-                     deleted=prev - curr) for prev, curr in zip(prev_scan, current_scan)]
-
+from observer import *
 
 scan = scanner('./test', 5)
 print(scan())
